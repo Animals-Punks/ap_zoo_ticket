1a20615daedbb69ab59618e4f7a3f8d2bc0c58b4
mo-zza <bigsmalljoe@gmail.com>
2021-11-30 10:01:21 +0900

    feat: ✨ ticket interface transfer

--- contract/Ticket/interfaces/ITiket.sol
+++ contract/Ticket/interfaces/ITiket.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.5.0;
 
 contract ITicket  {
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+	event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+	event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
     function mintToken(address to) public;
     function safeTrnsferFrom(address from, address to, uint256 tokenId) public;
     function setMinterContract(address saleContract) public;
